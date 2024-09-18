ea4e3a58ac441d8cd145ac8ab3b1566395ab528d
Camila-rodriguezz <a.22308051281300@cbtis128.edu.mx>
2024-09-18 07:39:32 -0600

    Add files via upload

---
+++ claseperro.py
@@ -0,0 +1,17 @@
+print("Programacion POO")
+# definicion de clase
+class Perro:
+    # funciones dentro de la clase
+    def morder(self):
+        print("El perro me mordio")
+    def Datos_perro(self,nombre,edad):
+        print(f"Nombre: {nombre} edad: {edad}")
+
+
+# zona de creacion de objeto
+pitbull=Perro()
+
+# zona de uso de objetos
+
+pitbull.Datos_perro("boby", 4)
+pitbull.morder()
